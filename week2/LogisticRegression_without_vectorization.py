e8a6073b7499ab1f5810b2491f1d6eb6fc9f9b5b
hipiphock <hipiphock@naver.com>
2020-03-24 16:35:38 +0900

    undated

--- week2/LogisticRegression_without_vectorization.py
+++ week2/LogisticRegression_without_vectorization.py
@@ -6,10 +6,10 @@ def sigmoid(x):
 
 def L(a, y):
     a = max(a, 1e-6)
-    return -(y*math.log(a) + (1-y)*math.log(1-a))
+    return -((y*math.log(a) + (1-y)*math.log(1-a)))
 
 
-# Step 1. Generate 1000(=m) train samples, 100(=n) test samples
+# Step 1. Generate 1000(=m) train samples,
 m = 1000
 n = 100
 x1_train=[]
@@ -22,6 +22,17 @@ for i in range(m):
         y_train.append(1)
     else:
         y_train.append(0)
+# Genrate 100(=n) test samples
+x1_test = []
+x2_test = []
+y_test = []
+for i in range(n):
+    x1_test.append(random.randint(-10, 10))
+    x2_test.append(random.randint(-10, 10))
+    if x1_train [-1] + x2_train[-1] > 0:
+        y_test.append(1)
+    else:
+        y_test.append(0)
 
 # generate 100 test
 # with learning rate alpha = 0.0001
@@ -39,40 +50,36 @@ print("Initial value for w1: {}, w2: {}, b: {}".format(w1, w2, b))
 loss = 0
 dw1 = 0
 dw2 = 0
+db = 0
 count = 0
 for j in range(n):
     for i in range(m):
         z = w1*x1_train[i] + w2*x2_train[i] + b
-        if sigmoid(z) > 0.5:
-            a = 1
+        # y_hat = a = sigmoid(z)
+        y_hat = sigmoid(z)
+        if y_hat > 0.5:
+            y_hat = 1
         else:
-            a = 0
-        print("Z: {}".format(z))
-        loss += L(a, y_train[i])
-        dw1 += x1_train[i]*(a - y_train[i])
-        dw2 += x2_train[i]*(a - y_train[i])
-        if a == y_train[i]:
-            count+=1
-    w1 = w1 - alpha*dw1
-    w2 = w2 - alpha*dw2
-    b = b - alpha*(a - y_train[i])
+            y_hat = 0
+        # print("Z: {}".format(z))
+        dzi = y_hat - y_train[i]
+        loss += L(y_hat, y_train[i])
+        dw1 += x1_train[i]*dzi
+        dw2 += x2_train[i]*dzi
+        db += dzi
+        # if a == y_train[i]:
+        #     count+=1
+    loss //= m
+    dw1 //= m
+    dw2 //= m
+    db //= m
+    w1 -= alpha*dw1
+    w2 -= alpha*dw2
+    b -= alpha*db
     print("w1: {}, w2: {}, b: {}".format(w1, w2, b))
     
 
 # Step 2.3. Calculate the cost with n test samples
-# First, create 100 test samples
-x1_test = []
-x2_test = []
-y_test = []
-for i in range(n):
-    x1_test.append(random.randint(-10, 10))
-    x2_test.append(random.randint(-10, 10))
-    if x1_train [-1] + x2_train[-1] > 0:
-        y_test.append(1)
-    else:
-        y_test.append(0)
-    z = w1*x1_test[i] + w2*x2_test[i] + b
-
 
 # Step 2.4. Print accuracy with m train samples 
 # (display the number of correctly predicted outputs / 1000*100)
@@ -85,7 +92,7 @@ for i in range(m):
         a = 0
     if a == y_train[i]:
         train_sum+=1
-    print("train accuracy: {}".format(train_sum//1000))
+print("train accuracy: {}".format(train_sum//m))
 
 # Step 2.5. Print accuracy with n test samples
 # (display the number of correctly predicted outputs / 100*100)
@@ -98,4 +105,4 @@ for i in range(n):
         a = 0
     if a == y_test[i]:
         test_sum+=1
-    print("test accuracy: {}".format(test_sum//100))
+print("test accuracy: {}".format(test_sum//n))
