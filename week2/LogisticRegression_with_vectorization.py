343e90ee9b32d2cf50ff583245a2edf9e8262c7d
hipiphock <hipiphock@naver.com>
2020-03-28 16:44:54 +0900

    Finished with both file, report left

--- week2/LogisticRegression_with_vectorization.py
+++ week2/LogisticRegression_with_vectorization.py
@@ -1,101 +1,96 @@
+import numpy as np
 import random
 import math
+from typing import List, Any
 
-def sigmoid(x):
-    return 1//(1+math.exp(-x))
+# INITAILIZATION
+MIN_NUMBER = 1e-6
 
-def L(a, y):
-    a = max(a, 1e-6)
-    return -(y*math.log(a) + (1-y)*math.log(1-a))
-
-
-# Step 1. Generate 1000(=m) train samples, 100(=n) test samples
 m = 1000
 n = 100
-x1_train=[]
-x2_train=[]
-y_train=[]
-for i in range(m):
-    x1_train.append(random.randint(-10, 10))
-    x2_train.append(random.randint(-10, 10))
-    if x1_train [-1] + x2_train[-1] > 0:
-        y_train.append(1)
-    else:
-        y_train.append(0)
 
-# generate 100 test
-# with learning rate alpha = 0.0001
-# if y_hat > 0.5 :
-#   set y_hat = 1   # for accuray evaluation
-
-# Step 2. Update w1, w2, b with 1000 samples for 100 iteration: #100 grad updates
-# You can use zeros for our logistic regression model.
-w1 = 0
-w2 = 0
-b = 0
-alpha = 0.0001
-# Step 2.2. Calculate the cost with m train samples
-print("Initial value for w1: {}, w2: {}, b: {}".format(w1, w2, b))
+alpha = 0.0001 # learning rate
 loss = 0
-dw1 = 0
-dw2 = 0
-count = 0
-for j in range(n):
-    for i in range(m):
-        z = w1*x1_train[i] + w2*x2_train[i] + b
-        if sigmoid(z) > 0.5:
-            a = 1
+
+w = [0, 0, 0]
+b = 0
+
+# data generator
+def generate_random_data(size):
+    datalist = []
+    for i in range(size):
+        data = []
+        x1 = random.randint(-10, 10)
+        x2 = random.randint(-10, 10)
+        data.append(x1)
+        data.append(x2)
+        if data[0] + data[1] > 0:
+            data.append(1)
         else:
-            a = 0
-        print("Z: {}".format(z))
-        loss += L(a, y_train[i])
-        dw1 += x1_train[i]*(a - y_train[i])
-        dw2 += x2_train[i]*(a - y_train[i])
-        if a == y_train[i]:
-            count+=1
-    w1 = w1 - alpha*dw1
-    w2 = w2 - alpha*dw2
-    b = b - alpha*(a - y_train[i])
-    print("w1: {}, w2: {}, b: {}".format(w1, w2, b))
-    
+            data.append(0)
+        datalist.append(data)
+    return datalist
 
-# Step 2.3. Calculate the cost with n test samples
-# First, create 100 test samples
-x1_test = []
-x2_test = []
-y_test = []
-for i in range(n):
-    x1_test.append(random.randint(-10, 10))
-    x2_test.append(random.randint(-10, 10))
-    if x1_train [-1] + x2_train[-1] > 0:
-        y_test.append(1)
-    else:
-        y_test.append(0)
-    z = w1*x1_test[i] + w2*x2_test[i] + b
+# Sigmoid function
+def sigmoid(x):
+    return 1/(1+math.exp(-x))
+
+# Loss function for Logistic Regression
+def L(a, y):
+    return -(y*math.log(a) + (1-y)*math.log(1-a))
+
+# training function
+def train(datalist):
+    global w, b
+    batch_dw1, batch_dw2, batch_db = 0, 0, 0
+    for x in datalist:
+        z = np.dot(w, x) + b
+        a = sigmoid(z)
+        da = -x[2]/a + (1-x[2])/(1-a)
+        dz = da * a * (1-a)
+        dw1 = x[0]*dz
+        dw2 = x[1]*dz
+        db = dz
+        batch_dw1 += dw1 / len(datalist)
+        batch_dw2 += dw2 / len(datalist)
+        batch_db += db / len(datalist)
+    w[0] -= alpha*batch_dw1
+    w[1] -= alpha*batch_dw2
+    b -= alpha*batch_db
 
 
-# Step 2.4. Print accuracy with m train samples 
-# (display the number of correctly predicted outputs / 1000*100)
-train_sum = 0
-for i in range(m):
-    z = w1*x1_train[i] + w2*x2_train[i] + b
-    if sigmoid(z) > 0.5:
-        a = 1
-    else:
-        a = 0
-    if a == y_train[i]:
-        train_sum+=1
-    print("train accuracy: {}".format(train_sum//1000))
+def forward(x):
+    global w, b
+    z = np.dot(w, x) + b
+    a = sigmoid(z)
+    MIN_VAL = 1e-10
+    a = max(a, MIN_VAL)
+    a = min(a, 1 - MIN_VAL)
+    return a
 
-# Step 2.5. Print accuracy with n test samples
-# (display the number of correctly predicted outputs / 100*100)
-test_sum = 0
-for i in range(n):
-    z = w1*x1_test[i] + w2*x2_test[i] + b
-    if sigmoid(z) > 0.5:
-        a = 1
-    else:
-        a = 0
-    if a == y_test[i]:
-        test_sum+=1
-    print("test accuracy: {}".format(test_sum//100))
+def loss(datalist):
+    batch_loss = 0
+    for x in datalist:
+        pred_y = forward(x)
+        batch_loss -= x[2] * math.log(pred_y) + (1 - x[2]) * math.log(1 - pred_y)
+    batch_loss /= len(datalist)
+    return batch_loss
+
+def accuracy(datalist):
+    num_correct = 0
+    for x in datalist:
+        z = z = np.dot(w, x) + b
+        a = sigmoid(z)
+        if x[2] == round(forward(x)):
+            num_correct += 1
+    return num_correct/len(datalist)
+
+if __name__ == '__main__':
+    train_data = generate_random_data(m)
+    test_data = generate_random_data(n)
+    for i in range(m):
+        train(train_data)
+        print("Iteration: "+ i.__str__())
+        print('w1: {}, w2: {}, b: {}'.format(w[0], w[1], b))
+        print('train loss: {}, accuracy: {}'.format(loss(train_data), accuracy(train_data)))
+        print('test loss: {}, accuracy: {}'.format(loss(test_data), accuracy(test_data)))
--- week2/LogisticRegression_without_vectorization.py
+++ week2/LogisticRegression_without_vectorization.py
@@ -1,111 +1,90 @@
 import random
 import math
+from typing import List, Any
 
+# INITAILIZATION
 MIN_NUMBER = 1e-6
 
+m = 1000
+n = 100
+
+alpha = 0.0001 # learning rate
+loss = 0
+
+w1, w2, b = 0, 0, 0
+
+# data generator
+def generate_random_data(size):
+    datalist = []
+    for i in range(size):
+        data = {'x1':random.randint(-10, 10), 'x2':random.randint(-10, 10)}
+        if data['x1'] + data['x2'] > 0:
+            data['y'] = 1
+        else:
+            data['y'] = 0
+        datalist.append(data)
+    return datalist
+
+# Sigmoid function
 def sigmoid(x):
     return 1/(1+math.exp(-x))
 
+# Loss function for Logistic Regression
 def L(a, y):
-    if a < MIN_NUMBER:
-        a = MIN_NUMBER
-    elif a >= 1:
-        a = 1- MIN_NUMBER
     return -(y*math.log(a) + (1-y)*math.log(1-a))
 
-# Step 1. Generate 1000(=m) train samples,
-m = 1000
-n = 100
-x1_train=[]
-x2_train=[]
-y_train=[]
-for i in range(m):
-    x1_train.append(random.randint(-10, 10))
-    x2_train.append(random.randint(-10, 10))
-    if x1_train [-1] + x2_train[-1] > 0:
-        y_train.append(1)
-    else:
-        y_train.append(0)
+# training function
+def train(datalist):
+    global w1, w2, b
+    batch_dw1, batch_dw2, batch_db = 0, 0, 0
+    for data in datalist:
+        z = w1*data['x1'] + w2*data['x2'] + b
+        a = sigmoid(z)
+        da = -data['y']/a + (1-data['y'])/(1-a)
+        dz = da * a * (1-a)
+        dw1 = data['x1']*dz
+        dw2 = data['x2']*dz
+        db = dz
+        batch_dw1 += dw1 / len(datalist)
+        batch_dw2 += dw2 / len(datalist)
+        batch_db += db / len(datalist)
+    w1 -= alpha*batch_dw1
+    w2 -= alpha*batch_dw2
+    b -= alpha*batch_db
 
-# Generate 100(=n) test samples
-x1_test = []
-x2_test = []
-y_test = []
-for i in range(n):
-    x1_test.append(random.randint(-10, 10))
-    x2_test.append(random.randint(-10, 10))
-    if x1_train[-1] + x2_train[-1] > 0:
-        y_test.append(1)
-    else:
-        y_test.append(0)
 
-# Step 2. Update w1, w2, b with 1000 samples for 100 iteration: #100 grad updates
-# You can use zeros for our logistic regression model.
-w1 = 0
-w2 = 0
-b = 0
-alpha = 0.0001 # learning rate
-# Step 2.2. Calculate the cost with m train samples
-print("Initial value for w1: {}, w2: {}, b: {}".format(w1, w2, b))
-loss = 0
-dw1 = 0
-dw2 = 0
-db = 0
-count = 0
-for j in range(n):
+def forward(x1, x2):
+    global w1, w2, b
+    z = w1 * x1 + w2 * x2 + b
+    a = sigmoid(z)
+    MIN_VAL = 1e-10
+    a = max(a, MIN_VAL)
+    a = min(a, 1 - MIN_VAL)
+    return a
+
+def loss(datalist):
+    batch_loss = 0
+    for data in datalist:
+        pred_y = forward(data['x1'], data['x2'])
+        batch_loss -= data['y'] * math.log(pred_y) + (1 - data['y']) * math.log(1 - pred_y)
+    batch_loss /= len(datalist)
+    return batch_loss
+
+def accuracy(datalist):
+    num_correct = 0
+    for data in datalist:
+        z = w1*data['x1'] + w2*data['x2'] + b
+        a = sigmoid(z)
+        if data['y'] == round(forward(data['x1'], data['x2'])):
+            num_correct += 1
+    return num_correct/len(datalist)
+
+if __name__ == '__main__':
+    train_data = generate_random_data(m)
+    test_data = generate_random_data(n)
     for i in range(m):
-        z = w1*x1_train[i] + w2*x2_train[i] + b
-        # y_hat = a = sigmoid(z)
-        y_hat = sigmoid(z)
-        if y_hat > 0.5:
-            y_hat = 1
-        else:
-            y_hat = 0
-        dzi = y_hat - y_train[i]
-        loss += L(y_hat, y_train[i])
-        dw1 += x1_train[i]*dzi
-        dw2 += x2_train[i]*dzi
-        db += dzi
-        if y_hat == y_train[i]:
-            count+=1
-    loss /= m
-    dw1 /= m
-    dw2 /= m
-    db /= m
-    w1 -= alpha*dw1
-    w2 -= alpha*dw2
-    b -= alpha*db
-    print("w1: {}, w2: {}, b: {}".format(w1, w2, b))
-    print("loss: {}".format(loss))
-    print("train accuracy: {}".format(count/m))
-    
-
-# Step 2.3. Calculate the cost with n test samples
-
-# Step 2.4. Print accuracy with m train samples 
-# (display the number of correctly predicted outputs / 1000*100)
-train_sum = 0
-for i in range(m):
-    z = w1*x1_train[i] + w2*x2_train[i] + b
-    y_hat = sigmoid(z)
-    if y_hat > 0.5:
-        y_hat = 1
-    else:
-        y_hat = 0
-    if y_hat == y_train[i]:
-        train_sum+=1
-print("train accuracy: {}".format(train_sum/m))
-
-# Step 2.5. Print accuracy with n test samples
-# (display the number of correctly predicted outputs / 100*100)
-test_sum = 0
-for i in range(n):
-    z = w1*x1_test[i] + w2*x2_test[i] + b
-    y_hat = sigmoid(z)
-    if y_hat > 0.5:
-        y_hat = 1
-    else:
-        y_hat = 0
-    if y_hat == y_test[i]:
-        test_sum+=1
-print("test accuracy: {}".format(test_sum/n))
+        train(train_data)
+        print("Iteration: "+ i.__str__())
+        print('w1: {}, w2: {}, b: {}'.format(w1, w2, b))
+        print('train loss: {}, accuracy: {}'.format(loss(train_data), accuracy(train_data)))
+        print('test loss: {}, accuracy: {}'.format(loss(test_data), accuracy(test_data)))
