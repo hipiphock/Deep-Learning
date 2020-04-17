bdd96d21540ab7ea5e688773dbfb619e831c525c
hingook@splab <hipiphock@naver.com>
2020-04-17 14:48:21 +0900

    finished train function

--- practice3/task3.py
+++ practice3/task3.py
@@ -37,22 +37,35 @@ def sigmoid(val):
 
 def train(X, Y):
     global W1, W2, B1, B2
+    dW1 = np.array([[0, 0], [0, 0], [0, 0]])
+    dW2 = np.array([0, 0, 0])
+    dB1 = np.array([0, 0, 0])
+    dB2 = 0
     # train each datum from train_data_X and train_data_Y
     for Xi, Yi in zip(X, Y):
         ### forwarding
         # for the first layer
         Z1 = np.dot(W1, Xi) + B1
-        A1 = map(Z1, sigmoid)
+        A1 = list(map(sigmoid, Z1))
         # for the second layer
         Z2 = np.dot(W2, A1) + B2
-        A2 = map(Z2, sigmoid)
+        A2 = sigmoid(Z2)
         ### back propagation
         # need to find out dL/dW2, dL/dW1, dL/dB2. dL/dB1
-        
-
-        
-
-
+        dL_dW2 = list(map(lambda x: x*(A2-Yi), A1))
+        dL_dB2 = (A2-Yi)*1
+        dL_dW1 = (A2-Yi)*(Z2*(1-Z2))*(A1*(1-A1))*(Xi)
+        dL_dB1 = (A2 - Yi) * (Z2*(1 - Z2)) * (A1*(1 - A1)) * (B1)
+        dW1 = dW1 + dL_dW1
+        dW2 = dW2 + dL_dW2
+        dB1 = dB1 + dL_dB1
+        dB2 = dB2 + dL_dB2
+    dW1 = dW1 / TRAIN_NUM
+    dW2 = dW2 / TRAIN_NUM
+    dB1 = dB1 / TRAIN_NUM
+    dB2 = dB2 / TRAIN_NUM
+    return dW1, dW2, dB1, dB2
+           
 if __name__ == '__main__':
     train_X, train_Y = generate_data(TRAIN_NUM)
     test_X, test_Y = generate_data(TEST_NUM)
--- practice3/test.py
+++ practice3/test.py
@@ -3,4 +3,8 @@ import numpy as np
 A = np.array([[0, 1], [1, 0], [0, 1]])
 B = np.array([1, 1])
 
+C = np.array([1, 2, 3])
+D = np.array([3, 2, 1])
+
 print(np.dot(A, B))
+print(np.dot(C, D))
